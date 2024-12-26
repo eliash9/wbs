39cffbb19e36747bd08862725536cbe4a52febd9
eliash <18025275+eliash9@users.noreply.github.com>
2024-12-26 15:18:22 +0700

    Update PairingCode.jsx

--- frontend/src/components/PairingCode.jsx
+++ frontend/src/components/PairingCode.jsx
@@ -8,7 +8,7 @@ const PairingForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:8989/pairing", {
+      const response = await fetch("https://wbs-pr8lla.fly.dev/pairing", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
